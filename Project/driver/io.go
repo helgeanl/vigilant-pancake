a515e5b410ac17652cb7f6033780e35e948adaa8
Student <student@itk-d650>
2016-02-10 12:03:02 +0100

    oi.c should be ported via io.go now

--- Project/driver/io.go
+++ Project/driver/io.go
@@ -8,3 +8,26 @@ package driver  // where "driver" is the folder that contains io.go, io.c, io.h,
 #include "io.h"
 */
 import "C"
+
+//Dropping "Io" prefix due to the "io." calling mechanism in GO
+func Init()int{    				
+	return C.io_init()
+}
+
+func SetBit(channel int){
+	return C.io_set_bit(channel)
+}
+
+func ClearBit(channel int){
+	return C.io_clear_bit(channel)
+}
+
+func WriteAnalog(channel int, value int){
+	return C.io_write_analog(channel, value)
+}
+func ReadBit(channel int)int{
+	return C.io_read_bit(channel)
+}
+func ReadAnalog(channel int)int {
+	return C.io_read_analog(channel)
+}
