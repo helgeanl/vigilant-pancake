1f97c9ac1e067127dd452eed535980910b4aab4e
helgeanl <helgelang@gmail.com>
2016-02-22 21:55:25 +0100

    Added exercise 7

--- Exercise6/main/Exercise6_Phoenix.go
+++ Exercise6/main/Exercise6_Phoenix.go
@@ -18,38 +18,29 @@ import(
 )
 
 func checkErr(err error){
-	if err != nil {
-		fmt.Println("An unrecovarable error occured", err.Error())
-		os.Exit(0)
-	}
+		if err != nil {
+			fmt.Println("An unrecovarable error occured", err.Error())
+			os.Exit(0)
+		}
 }
 
 func main(){
 
-	_,filePath,_,_ :=runtime.Caller(0)
-	directory, _ := filepath.Split(filePath)
+		_,filePath,_,_ :=runtime.Caller(0)
+		directory, _ := filepath.Split(filePath)
   	const iAmAliveInterval = 1 * time.Second
 		const iAmAliveTimeout = 3 * time.Second
-		 storageName := directory + "/Counter.dat"
-		 fmt.Println("Directory: " + directory )
-		 fmt.Println("Path: " + filePath )
+		storageName := directory + "/Counter.dat"
 		var counter = 0
 
-  print(string("----- Backup Phase -----\n"))
-	// open output file
-	storage, err := os.Open(storageName)
-	if err != nil {
-		newFile,_ := os.Create(storageName)
-		newFile.Close()
-		//checkErr(err)
-	}
-	// close fo on exit and check for its returned error
-//	defer func() {
-//		if err := storage.Close(); err != nil {
-//			panic(err)
-//		}
-//	}()
-
+  	print(string("----- Backup Phase -----\n"))
+		// open output file
+		storage, err := os.Open(storageName)
+		if err != nil {
+			newFile,_ := os.Create(storageName)
+			newFile.Close()
+			//checkErr(err)
+		}
 
 		for(true){
 			fmt.Println("...Waiting")
@@ -66,11 +57,10 @@ func main(){
 		// Run a new terminal window with backup
 		app := "osascript"
 		arg0 := "-e"
-		arg1 := "tell application \"Terminal\" to do script \"go run '"+filePath+"'\""
+		arg1 := "tell app \"Terminal\" to do script \"go run '"+filePath+"'\""
 		cmd := exec.Command( app,arg0,arg1)
 
 		cmd.Start(); // Carefull...
-
 
 		r4 := bufio.NewReader(storage)
     b4, _ := r4.Peek(8)
@@ -78,15 +68,16 @@ func main(){
     counter,_ = strconv.Atoi(string(b4))
 		storage.Close()
 		storage,_=os.Create(storageName)
+
 		defer func() {
 				if err := storage.Close(); err != nil {
 					panic(err)
 				}
 			}()
+
     for(true){
         counter++;
 				counterString := strconv.Itoa(counter)
-        //storage.Write(counter)
 				ioutil.WriteFile(storageName, []byte(counterString), 0644)
 				//w := bufio.NewWriter(storage)
 		  	///_,err := w.WriteString(counterString)
@@ -94,5 +85,4 @@ func main(){
         fmt.Println(counter)
         time.Sleep(iAmAliveInterval)
     }
-
 }
