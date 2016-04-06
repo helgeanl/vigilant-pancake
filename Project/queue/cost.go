4a652f80b1af27730b49f939beb9be8791da19f5
helgeanl <helgelang@gmail.com>
2016-04-06 19:24:57 +0200

    ...

---
+++ Project/queue/cost.go
@@ -0,0 +1,52 @@
+package queue
+
+import (
+	"config"
+	"fmt"
+)
+
+//CalcCost calculates the total amount of work it takes to reach the 
+//given floor. 
+func CalcCost(currDir, currFloor, prevFloor, targetFloor, targetBtn int) int{
+	totCost := 0
+	dir 	:= currDir
+	//Is the target above or below
+	targetDir := targetFloor - prevFloor
+
+
+	if currFloor == -1 {
+		//Elevator is between floors, +1 cost
+		totCost++
+	else if dir != config.DirStop {
+		//Elevator is at floor but not IDLE +2 cost
+		totCost += 2
+	}
+	if dir != config.DirStop {
+		if Targetdir != dir {
+			//if the elevator must switch direction, +10 cost
+			totCost += 10
+		}
+	}
+	if 
+	//Add +1 cost for every stop on the way to target
+	//I asume here that currFloor==prevFloor if the elevator is at
+	// a floor
+	if targetDir < 0 && dir == config.DirUp{
+		for floor := prevFloor; floor < targetFloor || floor == config.Numfloors; floor++{
+			if isOrder(floor, config.Btn) || isOrder(floor, config.BtnInside){
+				totCost ++
+			}
+			totCost++
+		}
+	}
+	if targetDir > 0 && dir == config.DirDown{
+		for floor := prevFloor; floor > targetFloor || floor == 0 ; floor--{
+			if isOrder(floor, targetBtn) || isOrder(floor, config.BtnInside){
+				totCost ++
+			}
+			totCost++
+		}
+	}
+
+	return totCost
+}
