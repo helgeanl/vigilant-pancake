f3577d073abfd13908887b5cf3c0bc7a7e244d17
Johan Vårlid <Johan Vrlid>
2016-04-10 13:52:00 +0200

    eventBtnPressed done

--- Project/main/event.go
+++ Project/main/event.go
@@ -11,25 +11,47 @@ func EventHandler(){
 	//Make convinient variables
 	//Fix lights
 	
+	//Convenient channels
 	var BtnChan = make(chan def.BtnPress, 10)
 
+
+	//Convenient variables/structs
+
+
+	//Threads
+	go eventBtnPressed(BtnChan)
+
+
 	for{
+		select{
+			case
+		}
 	}
 }
 
-func eventBtnPressed(){
+func eventBtnPressed(ch chan){
 	//Check for a button beeing pressed
-	lastBtnPresse := def.BtnPress{
-				Button: btn,
-				Floor: floor,
+	lastBtnPressed := def.BtnPress{
+		Button: -1,
+		Floor: -1,
 	} 
-	var BtnPressed:=-2
-	
-	for floor := 0; floor < def.NumFloors; floor++ {
-		for btn := 0; btn < def.NumButtons; btn++ {
-			if hw.ReadBtn(floor, btn){
-
+	btnPressed := def.BtnPress{
+		Button: -2,
+		Floor: -2,
+	}
+	for{
+		for floor := 0; floor < def.NumFloors; floor++ {
+			for btn := 0; btn < def.NumButtons; btn++ {
+				if hw.ReadBtn(floor, btn){
+					btnPressed.Button = btn
+					btnPressed.Floor = floor
+					//Check if there is an order assigned to button
+					if lastBtnPressed != btnPressed && !qMatrix.Active{
+						ch <- btnPressed
+					}
+				}
 			}
 		}
+		lastBtnPressed = btnPressed
 	}
 }
