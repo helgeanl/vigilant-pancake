376290f5132f5894d68228d5a50d955238f5bb9c
powermundsen <andrenw@stud.ntnu.no>
2016-04-11 21:55:23 +0200

    ..

--- Project/src/definitions/definitions.go
+++ Project/src/definitions/definitions.go
@@ -9,12 +9,11 @@ import (
 // Global system constants
 const NumButtons = 3
 const NumFloors = 4
-const NumElevators = 3
 const DoorOpenTime = 3 * time.Second
 const SpamInterval = 400 * time.Millisecond
 const CostReplyTimeoutDuration = 10 * time.Second
 const RequestTimeoutDuration = 30 * time.Second
-const DeathConfirmedTime = 3 * time.Second
+const ElevTimeoutDur = 3 * time.Second
 
 const (
 	BtnHallUp int = iota
